a9086bae8ffcce365fd982588d146e82938c249c
Juan I Condina <43761363+jcondina@users.noreply.github.com>
2023-08-29 19:58:03 -0300

    CV refactors

---
+++ src/consts.ts
@@ -0,0 +1,70 @@
+export const certifications = [
+	{
+		name: "Responsive Web Design by freeCodeCamp",
+		url: "https://www.freecodecamp.org/certification/fcc5d67b6a7-3c54-4347-a692-16f5cf9348ac/responsive-web-design",
+	},
+	{
+		name: "Enterprise Design Thinking Practitioner by IBM",
+		url: "https://www.credly.com/badges/08bb8c20-0c71-4ef8-935a-4b8cc97903cf",
+	},
+];
+
+export const education = [
+	{
+		title: "Information Systems Engineering",
+		subtitle: "2019 - Present at Universidad Tecnologica Nacional, Buenos Aires, Argentina",
+	},
+	{
+		title: "Electronics Engineering",
+		subtitle: "2013 to 2018 at Universidad Tecnologica Nacional, Buenos Aires, Argentina",
+		description: "Made it to the 3rd year of the career. I decided to change to Information Systems Engineering.",
+	},
+];
+
+export const experiences = [
+	{
+		title: "Full Stack Developer",
+		subtitle: "From November 2021 to June 2023 at Modus Agency, Remote",
+		link: "https://modusagency.com/",
+	},
+	{
+		title: "Full Stack Developer",
+		subtitle: "From March 2021 to November 2021 at Devlights, Remote",
+		link: "https://devlights.com/",
+	},
+	{
+		title: "AWS Developer",
+		subtitle: "From December 2020 to March 2021 at CLEAR TECH, Remote",
+		link: "https://clear-tech.com/",
+	},
+	{
+		title: "Full Stack Developer",
+		subtitle: "From October 2018 to December 2020 at SOUTHWORKS, Buenos Aires, Argentina",
+		link: "https://southworks.com",
+		description:
+			"Worked on a variety of projects for different clients, including a large US-based bank, a US-based insurance company, and a US-based healthcare company.",
+	},
+];
+
+export const skills = [
+	"HTML",
+	"CSS",
+	"JavaScript",
+	"React.js",
+	"Next.js",
+	"Tailwind",
+	"Astro",
+	"NodeJS",
+	"NestJS",
+	"Python",
+	"SQL",
+	"NoSQL",
+	"AWS",
+	"Azure",
+	"FireBase",
+	"Git",
+	"VSCode",
+	"Linux",
+	"Office",
+	"Docker",
+];

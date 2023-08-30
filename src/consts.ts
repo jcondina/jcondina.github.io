3ff556bf770c2e83037555def98e1350541905fb
Juan I. Condina <juani.condina@gmail.com>
2023-08-30 02:13:12 -0300

    Finish working on CV page

--- src/consts.ts
+++ src/consts.ts
@@ -26,23 +26,25 @@ export const experiences = [
 		title: "Full Stack Developer",
 		subtitle: "From November 2021 to June 2023 at Modus Agency, Remote",
 		link: "https://modusagency.com/",
+		description: `Gibson webpage collaborative development and maintenance. Collaboratively developed Moody's web applications for KYS and supply chain management. Collaboratively developed Coleridge Initiative web application for AWS resources management. Collaboratively developed Coleridge Initiative webpage. Collaboratively developed ADP web application for agents profiles.`,
 	},
 	{
 		title: "Full Stack Developer",
 		subtitle: "From March 2021 to November 2021 at Devlights, Remote",
 		link: "https://devlights.com/",
+		description: `Webpage maintenance, migration and improvement on my own. Responsive restaurant webapp to handle QR menus, reservation and delivery services development on my own. Both projects developed working side to side with client and held meetings to discuss performance and priorities.`,
 	},
 	{
 		title: "AWS Developer",
 		subtitle: "From December 2020 to March 2021 at CLEAR TECH, Remote",
 		link: "https://clear-tech.com/",
+		description: `Collaboratively developed a workforce automation app that handles call center employee's status in near real time. Achieved the goal by using Lambda to subscribe to a SQS event and then using CloudWatch to monitor the metrics for the Lambda function. Discussed with client to satisfy workforce automation project needs. Was able to finish sprints with extra time which used to discuss on project architecture.`,
 	},
 	{
 		title: "Full Stack Developer",
 		subtitle: "From October 2018 to December 2020 at SOUTHWORKS, Buenos Aires, Argentina",
 		link: "https://southworks.com",
-		description:
-			"Worked on a variety of projects for different clients, including a large US-based bank, a US-based insurance company, and a US-based healthcare company.",
+		description: `Microsoft Azure Portal collaborative development and testing with a team. Collaboratively developed with a team an IoT project using Microsoft Azure services and microcontrollers for Microsoft. Collaboratively developed with a team a Microsoft Azure Cognitive Service sample featured in https://github.com/Azure-Samples/cognitive-services-personalizer-samples/tree/master/samples/CrawlFeaturizer. Collaboratively developed with a team a Microsoft Azure Services deployment webapp for education industry resulting in great client satisfaction. Collaboratively developed with a team a real time video streaming and monitoring platform for Discovery resulting in great client satisfaction and even featured in AWS re:Invent 2019.`,
 	},
 ];
 
